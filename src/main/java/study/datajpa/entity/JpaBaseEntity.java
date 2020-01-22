de60dfac28328ae82c6ccee8123fa8dc0a9ac803
YeseulK <yeseullk@gmail.com>
2020-01-22 16:26:13 +0900

    Add Auditing

---
+++ src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,35 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+/**
+ * <Auditing>
+ * - 엔티티를 생성, 변경할 때 변경한 사람과 시간을 추적하고 싶을 때
+ * 등록일, 수정일, 등록자, 수정자
+ */
+@MappedSuperclass
+@Getter
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+}
